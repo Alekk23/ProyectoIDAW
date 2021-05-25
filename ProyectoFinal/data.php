a13b43fba401d6cb34e37a3471ebb8d271a0c7a8
alain <alainedw2046@outlook.com>
2021-05-25 15:20:23 -0500

    Hi4

--- ProyectoFinal/data.php
+++ ProyectoFinal/data.php
@@ -10,7 +10,7 @@
 	$file = $_POST['pago'];
 
 	// Database connection
-	$conn = new mysqli('localhost','root','ProgramacionPOO','test');
+	$conn = new mysqli('localhost','root','ProgramacionPOO','alumnopago');
 	if($conn->connect_error){
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
