0be83494bafd4a4ff487b3fa0ea03b6ec0f07841
alain <alainedw2046@outlook.com>
2021-05-25 16:43:37 -0500

    Hi5

--- ProyectoFinal/data.php
+++ ProyectoFinal/data.php
@@ -1,11 +1,11 @@
 <?php
-	$whPaterno = $_POST['primApell'];
+	$whPaterno = $_POST["primApell"];
 	$whMaterno = $_POST['secApell'];
 	$whNombre = $_POST['nombre'];
 	$whFecNac = $_POST['fecha'];
 	$whGenero = $_POST['genero'];
 	$whNacion = $_POST['lugar'];
-	$whEntNac = $_POST['lugar'];
+	$whEntNac = $_POST['lugarmun'];
 	$whCurp = $_POST['curp'];
 	$file = $_POST['pago'];
 
@@ -15,12 +15,11 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(primApell, secApell, nombre, fecha, genero, lugar, lugarmun, curp, pago) values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
-		$stmt->bind_param("sssisssss", $whPaterno, $whMaterno, $whNombre, $whFecNac, $whGenero, $whNacion, $whEntNac, $whCurp, $whCurp);
+		$stmt = $conn->prepare("insert into comprobante(primApell, secApell, nombre, fecha, genero, lugar, lugarmun, curp, pago) values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
+		$stmt->bind_param("sssssssss", $whPaterno, $whMaterno, $whNombre, $whFecNac, $whGenero, $whNacion, $whEntNac, $whCurp, $whCurp);
 		$execval = $stmt->execute();
 		echo $execval;
 		echo "Registration successfully...";
 		$stmt->close();
 		$conn->close();
 	}
-?>
