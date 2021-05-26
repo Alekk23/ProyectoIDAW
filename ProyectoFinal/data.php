abed5255bc5fb0e1e323a0a3b7b72a4f82df63e9
alain <alainedw2046@outlook.com>
2021-05-26 15:46:29 -0500

    HI100

--- ProyectoFinal/data.php
+++ ProyectoFinal/data.php
@@ -1,25 +1,35 @@
 <?php
+include('ProyectoFinal.html');
 
 $con = mysqli_connect('localhost', 'root', 'ProgramacionPOO', 'alumnopago');
 
-$whPaterno = $_POST['primApell'];
-$whMaterno = $_POST['secApell'];
-$whNombre = $_POST['nombre'];
-$whFecNac = $_POST['fecha'];
-$whGenero = $_POST['genero'];
-$whNacion = $_POST['lugar'];
-$whEntNac = $_POST['lugarmun'];
-$whCurp = $_POST['curp'];
-$file = $_POST['pago'];
+$whPaterno = $_POST['whPaterno'];
+$whMaterno = $_POST['whMaterno'];
+$whNombre = $_POST['whNombre'];
+$whFecNac = $_POST['whFecNac'];
+$whGenero = $_POST['whGenero'];
+$whNacion = $_POST['whNacion'];
+$whEntNac = $_POST['whEntNac'];
+$whCurp = $_POST['whCurp'];
+$target_dir = "uploads/";
+$target_file = $target_dir . basename($_FILES["file"]["name"]);
+$uploadOk = 1;
+$imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
 
 
-$sql = "INSERT INTO `comprobante`(`id`, `primApell`, `secApell`, `nombre`, `fecha`, `genero`, `lugar`, `lugarmun`, `curp`, `pago`) VALUES ('0',  '$whPaterno', '$whMaterno', '$whNombre', '$whFecNac', '$whGenero', '$whNacion', '$whEntNac', '$whCurp', '$file')";
+$sql = "INSERT INTO `comprobante`(`id`, `primApell`, `secApell`, `nombre`, `fecha`, `genero`, `lugar`, `lugarmun`, `curp`, `pago`) VALUES ('0',  '$whPaterno', '$whMaterno', '$whNombre', '$whFecNac', '$whGenero', '$whNacion', '$whEntNac', '$whCurp', '$target_file')";
 
 
 $rs = mysqli_query($con, $sql);
 
-if ($rs) {
-    echo "Contact Records Inserted";
+if($rs){
+
+    echo "<script> alert('Registro Correcto');
+    location.href = 'ProyectoFinal.html';
+   </script>";
+
+}else{
+    echo "<script> alert('Registro incorrecto');
+    location.href = 'ProyectoFinal.html';
+    </script>";
 }
-
-
