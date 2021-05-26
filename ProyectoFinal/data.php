348012c39e19ad27386efead0171fcf8dc15e5e4
alain <alainedw2046@outlook.com>
2021-05-26 14:14:14 -0500

    hi11

--- ProyectoFinal/data.php
+++ ProyectoFinal/data.php
@@ -1,27 +1,28 @@
 <?php
-        $whPaterno = $_POST['primApell'];
-        $whMaterno = $_POST['secApell'];
-        $whNombre = $_POST['nombre'];
-        $whFecNac = $_POST['fecha'];
-        $whGenero = $_POST['genero'];
-        $whNacion = $_POST['lugar'];
-        $whEntNac = $_POST['lugarmun'];
-        $whCurp = $_POST['curp'];
-        $file = $_POST['pago'];
+// database connection code
+// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
+$con = mysqli_connect('localhost', 'root', 'ProgramacionPOO', 'alumnopago');
 
-        // Database connection
-        $conn = new mysqli('localhost', 'root', 'ProgramacionPOO', 'alumnopago');
-        if ($conn->connect_error) {
-            echo "$conn->connect_error";
-            die("Connection Failed : " . $conn->connect_error);
-        } else {
-            $stmt = $conn->prepare("insert into comprobante(primApell, secApell, nombre, fecha, genero, lugar, lugarmun, curp, pago) values(?, ?, ?, ?, ?, ?, ?, ?, ?)");
-            $stmt->bind_param("sssssssss", $whPaterno, $whMaterno, $whNombre, $whFecNac, $whGenero, $whNacion, $whEntNac, $whCurp, $whCurp);
-            $execval = $stmt->execute();
-            echo $execval;
-            echo "Registration successfully...";
-            $stmt->close();
-            $conn->close();
-        }
+// get the post records
+$whPaterno = $_POST['primApell'];
+$whMaterno = $_POST['secApell'];
+$whNombre = $_POST['nombre'];
+$whFecNac = $_POST['fecha'];
+$whGenero = $_POST['genero'];
+$whNacion = $_POST['lugar'];
+$whEntNac = $_POST['lugarmun'];
+$whCurp = $_POST['curp'];
+$file = $_POST['pago'];
+
+// database insert SQL code
+$sql = "INSERT INTO `comprobante`(`id`, `primApell`, `secApell`, `nombre`, `fecha`, `genero`, `lugar`, `lugarmun`, `curp`, `pago`) VALUES ('0',  '$whPaterno', '$whMaterno', '$whNombre', '$whFecNac', '$whGenero', '$whNacion', '$whEntNac', '$whCurp', '$file')";
+
+// insert in database
+$rs = mysqli_query($con, $sql);
+
+if ($rs) {
+    echo "Contact Records Inserted";
+}
+
 
