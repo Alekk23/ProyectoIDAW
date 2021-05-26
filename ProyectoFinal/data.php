4b7d1614c1524f3026037839189f50d6b99fc61b
alain <alainedw2046@outlook.com>
2021-05-26 14:27:29 -0500

    Hi99

--- ProyectoFinal/data.php
+++ ProyectoFinal/data.php
@@ -1,10 +1,7 @@
 <?php
-// database connection code
-// $con = mysqli_connect('localhost', 'database_user', 'database_password','database');
 
 $con = mysqli_connect('localhost', 'root', 'ProgramacionPOO', 'alumnopago');
 
-// get the post records
 $whPaterno = $_POST['primApell'];
 $whMaterno = $_POST['secApell'];
 $whNombre = $_POST['nombre'];
@@ -15,10 +12,10 @@ $whEntNac = $_POST['lugarmun'];
 $whCurp = $_POST['curp'];
 $file = $_POST['pago'];
 
-// database insert SQL code
+
 $sql = "INSERT INTO `comprobante`(`id`, `primApell`, `secApell`, `nombre`, `fecha`, `genero`, `lugar`, `lugarmun`, `curp`, `pago`) VALUES ('0',  '$whPaterno', '$whMaterno', '$whNombre', '$whFecNac', '$whGenero', '$whNacion', '$whEntNac', '$whCurp', '$file')";
 
-// insert in database
+
 $rs = mysqli_query($con, $sql);
 
 if ($rs) {
